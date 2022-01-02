8510602895f3255c8b64c61cabd542819868a825
JuliaBrening <76996469+JuliaBrening@users.noreply.github.com>
2022-01-02 12:22:12 +0100

    About Page überarbeitet

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    "# Projekt-Suluhan-Brening"
+
+    Thema: Webbasierte To-Do-List Anwendung
+
+    Teammitglieder: Serkan Suluhan - 577512, Julia Brening - 579035
+
+    Studierende sind im Studium oft mit Einhaltung von Fristen beschäftigt. Egal ob Abgaben, Hausarbeiten oder das Treffen mit Kommilitonen in der Mensa, nicht selten kommt es vor, dass Fristen vergessen werden.
+
+    Daher werden wir im Wintersemester 2021/2022 eine webbasierte To-Do-Liste-Anwendung als Projekt in Webtechnologien programmieren, um Studierenden zu ermöglichen, den Alltag produktiver und strukturierter zu organisieren. Nicht selten gibt es spontane Veränderungen. Um ein optimales Nutzererlebnis zu erreichen, werden wir auch Funktionen einbauen, mit denen dynamische Veränderungen in der Webanwendung einfach und schnell umgesetzt werden können.
+
+    Themenbeschreibung: To-Do-Listen-Anwendung mit folgenden Funktionen:
+
+    To-Do-Notiz erstellen,
+    To-Do-Notiz löschen,
+    To-Do-Notiz abhaken,
+    To-Do-Notiz priorisieren,
+    To-Do-Notiz einem Datum zuordnen
   </div>
 </template>
