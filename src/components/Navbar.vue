749ec5038ce0aa8d6089b204ce9cfb9f414f5cf5
JuliaBrening <76996469+JuliaBrening@users.noreply.github.com>
2022-01-02 10:03:23 +0100

    Navbar überarbeitet und Icon hinzugefügt

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,14 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-dark bg-dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Plan Your Day</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand d-flex align-items-center text-white h3" href="#">
+        <img class="d-inline-block me-3" src="~@/assets/197717.png" alt="" style="width: 40px;">
+        Plan Your Day | <span class="fw-light ms-2">Strukturiere deinen Tag mit Plan Your Day</span>
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="collapse navbar-collapse" id="navbarNav">
         <div class="navbar-nav">
           <router-link class="nav-link" to="/">Home</router-link>
           <router-link class="nav-link" to="/about">About</router-link>
