eb6630b9496a3bf343be091e4799e362134cf885
JuliaBrening <76996469+JuliaBrening@users.noreply.github.com>
2022-01-09 12:21:01 +0100

    About / Read Me

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,22 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    "# Projekt-Suluhan-Brening"
+  <div class="about container mt-3">
+    <h1>Projekt-Suluhan-Brening</h1>
 
-    Thema: Webbasierte To-Do-List Anwendung
+    <h2>Thema: Webbasierte To-Do-List Anwendung</h2>
 
-    Teammitglieder: Serkan Suluhan - 577512, Julia Brening - 579035
+    <h6>Teammitglieder: Serkan Suluhan - 577512, Julia Brening - 579035</h6>
 
-    Studierende sind im Studium oft mit Einhaltung von Fristen beschäftigt. Egal ob Abgaben, Hausarbeiten oder das Treffen mit Kommilitonen in der Mensa, nicht selten kommt es vor, dass Fristen vergessen werden.
+    <div class="container-xxlmt-3">
 
-    Daher werden wir im Wintersemester 2021/2022 eine webbasierte To-Do-Liste-Anwendung als Projekt in Webtechnologien programmieren, um Studierenden zu ermöglichen, den Alltag produktiver und strukturierter zu organisieren. Nicht selten gibt es spontane Veränderungen. Um ein optimales Nutzererlebnis zu erreichen, werden wir auch Funktionen einbauen, mit denen dynamische Veränderungen in der Webanwendung einfach und schnell umgesetzt werden können.
+    Studierende sind im Studium oft mit Einhaltung von Fristen beschäftigt.
+    Egal ob Abgaben, Hausarbeiten oder das Treffen mit Kommilitonen in der Mensa, nicht selten kommt es vor, dass Fristen vergessen werden.
+    Daher haben wir im Wintersemester 2021/2022 eine webbasierte To-Do-Liste-Anwendung als Projekt in Webtechnologien programmiert, um es Studierenden zu ermöglichen, den Alltag produktiver und strukturierter zu organisieren.
+    Nicht selten gibt es spontane Veränderungen - diese lassen sich optimal mit Plan Your Day umsetzen. Um ein optimales Nutzererlebnis zu erreichen, werden wir auch Funktionen einbauen, mit denen dynamische Veränderungen in der Webanwendung einfach und schnell umgesetzt werden können.
 
-    Themenbeschreibung: To-Do-Listen-Anwendung mit folgenden Funktionen:
+      <h6>Themenbeschreibung: To-Do-Listen-Anwendung mit folgenden Funktionen:</h6>
 
-    To-Do-Notiz erstellen,
-    To-Do-Notiz löschen,
-    To-Do-Notiz abhaken,
-    To-Do-Notiz priorisieren,
-    To-Do-Notiz einem Datum zuordnen
+      <li>To-Do-Notiz erstellen,</li>
+      <li>To-Do-Notiz löschen,</li>
+      <li>To-Do-Notiz bearbeiten,</li>
+      <li>To-Do-Notiz priorisieren,</li>
+      <li>To-Do-Notizen nach Datum und Modulen filtern</li>
+      <li>To-Do-Notizen sortieren (indem man auf die entsprechende Tabellenspalte klickt)</li>
+    </div>
   </div>
 </template>
